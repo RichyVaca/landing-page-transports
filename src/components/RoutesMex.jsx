a19ffecf867c690bbfb58aeeb0d5ccebbd0dbd12
Ricardo Hiram Morales Vaca <ric_vaca_9519@outlook.com>
2024-08-26 09:52:02 -0600

    Upgrade

--- src/components/RoutesMex.jsx
+++ src/components/RoutesMex.jsx
@@ -1,36 +1,76 @@
-import { MDBCol, MDBContainer, MDBListGroup, MDBListGroupItem, MDBRow } from "mdb-react-ui-kit";
+import {
+  MDBCol,
+  MDBContainer,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBRow,
+} from "mdb-react-ui-kit";
 import React from "react";
-import mex from "../assets/mex2.jpg";
+import mex from "../assets/mex.png";
 
 const RoutesMex = () => {
   return (
     <>
       <div
-        className="my-4 text-center bg-image "
-        style={{
-          backgroundImage: `url(${mex})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
+      // className="my-4 text-center bg-image "
+      // style={{
+      //   backgroundImage: `url(${mex})`,
+      //   backgroundSize: "30rem",
+      //   backgroundPosition: "center",
+      // }}
       >
         <MDBContainer>
           <MDBRow className="m-3 p-5 ">
-            <MDBCol size="md opacity-90">
+            <MDBCol size="md opacity-70">
+              <h6 className="bg-light p-2 border-top border-bottom">
+                JALISCO
+              </h6>
               <MDBListGroup className="">
-                <MDBListGroupItem className="" color="dark" action>GUADALAJARA</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>MANZANILLO</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>MICHOACAN</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>TLAXCALA</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>MONTERREY</MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  GUADALAJARA
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  CD. GUZMAN
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  ZAPOPAN
+                </MDBListGroupItem>
+              </MDBListGroup>
+              <h6 className="bg-light p-2 mt-2 border-top border-bottom">
+                MICHOACAN
+              </h6>
+              <MDBListGroup>
+                <MDBListGroupItem className="" color="primary" action>
+                  MI RANCHITO
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  MORELIA
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  LAZARO CARDENAS
+                </MDBListGroupItem>
               </MDBListGroup>
             </MDBCol>
-            <MDBCol size="md opacity-90">
+            <MDBCol size="md opacity-70">
+            <h6 className="bg-light p-2 border-top border-bottom">
+                OTROS
+              </h6>
               <MDBListGroup className="">
-                <MDBListGroupItem className="" color="dark" action>PUEBLA</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>TECOMAN</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>...</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>...</MDBListGroupItem>
-                <MDBListGroupItem className="" color="dark" action>...</MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  PUEBLA
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  TLAXCALA
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  ...
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  ...
+                </MDBListGroupItem>
+                <MDBListGroupItem className="" color="primary" action>
+                  ...
+                </MDBListGroupItem>
               </MDBListGroup>
             </MDBCol>
           </MDBRow>
--- src/components/ServiceBox.jsx
+++ src/components/ServiceBox.jsx
@@ -4,15 +4,15 @@ import React from "react";
 const ServiceBox = ({ name, icon }) => {
   return (
     <MDBCol
-      size="md"
-      className="d-flex align-items-center justify-content-center p-3 m-3 square rounded-5 bg-secondary shadow-5-strong bg-opacity-25"
+      size="lg"
+      className="d-flex align-items-center justify-content-center p-3 m-3 square rounded-5 bg-secondary shadow-4-strong bg-opacity-25"
     >
       <h5>
         <MDBIcon
           fas
           icon={icon}
           className="d-flex justify-content-center"
-          size="2x"
+          size="1x"
         />
         <br />
         {name}
